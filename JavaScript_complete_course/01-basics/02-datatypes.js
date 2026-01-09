66ea2c0b0bc51277056427f77ee1456731a444e7
Shivam Kumar <shivam.builds071@gmail.com>
2026-01-09 08:39:38 +0530

    datatypes added

---
+++ JavaScript_complete_course/01-basics/02-datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello"); // we are using nodeJS not browser
+
+// 1. Primitive DataType
+
+// Number => 2 to power 53
+// BigInt
+// string = ""
+// Boolean => true / false
+// null => standAlone value
+// undefined
+// Symbol
+
+// Object.
+console.log(typeof "shivam");
+console.log(typeof 20);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
