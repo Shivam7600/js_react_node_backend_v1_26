88091e73187468725e01202e6e8f0163702f63cd
Shivam Kumar <shivam.builds071@gmail.com>
2026-01-11 04:51:16 +0530

    object file added

---
+++ JavaScript_complete_course/02-arrays_objects/03-objects.js
@@ -0,0 +1,45 @@
+// singleton => used and made by constructor function,
+// Object.create() -> constructor method and singleton.
+
+// literal vs construtor
+
+// object literal
+
+const mySym = Symbol("key1");
+
+const jsUser = {
+  name: "shivam",
+  "Full Name": "Shivam Kumar",
+  [mySym]: "myKey1value", // symbol as key
+  age: 18,
+  email: "shivamkumar071@gmail.com",
+  isLogegedIn: false,
+  lastLoginDays: ["Monday", "Friday"],
+};
+
+console.log(jsUser.name);
+console.log(jsUser["email"]);
+
+console.log(jsUser[mySym]);
+
+// update
+
+jsUser.email = "shivam@microsoft.com";
+console.log(jsUser.email); // email = "shivam@microsoft.com"
+
+// Object.freeze(jsUser); // prevent any changes to object
+
+jsUser.email = "shivam@chatgpt.com"; // email = "shivam@microsoft.com"
+console.log(jsUser.email); // no change
+
+// 1. -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+jsUser.greeting = function () {
+  console.log("Hello jsUser");
+};
+
+jsUser.greeting2 = function () {
+  console.log(`Hello ${this.name}`);
+};
+
+jsUser.greeting(); // Hello jsUser
+jsUser.greeting2(); // Hello shivam
